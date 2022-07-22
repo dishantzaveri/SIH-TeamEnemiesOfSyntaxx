d2663257f5c8bca780d215366b6e7e9f6caf04a3
grehashah6 <grehashah6@gmail.com>
2022-07-22 23:38:47 +0530

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Manan
+          Greha
         </p>
         <span>
           <span>Learn </span>
