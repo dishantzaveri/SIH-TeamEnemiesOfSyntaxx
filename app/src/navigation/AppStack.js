ef5267a4d67359b91bf7602f97c2e3818d749b92
dishantzaveri <zaveridishant@gmail.com>
2022-08-08 01:50:45 +0530

    video screens

--- app/src/navigation/AppStack.js
+++ app/src/navigation/AppStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomDrawer from '../components/CustomDrawer';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -23,6 +23,7 @@ import Onboarding3 from '../pages/Onboarding3';
 import Onboarding4 from '../pages/Onboarding4';
 import Onboarding5 from '../pages/Onboarding5';
 import Onboarding6 from '../pages/Onboarding6';
+import Careertv from '../pages/Careertv';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -42,22 +43,22 @@ function Tabs() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={({route}) => ({
+      screenOptions={({ route }) => ({
         tabBarIcon: () => icons[route.name],
       })}
-      // tabBarOptions={{
-      //   activeTintColor: '#18a990',
-      //   inactiveTintColor: 'gray',
-      //   labelStyle: {
-      //     fontWeight: 'bold',
-      //     fontSize: 12,
-      //   },
-      // }}
+    // tabBarOptions={{
+    //   activeTintColor: '#18a990',
+    //   inactiveTintColor: 'gray',
+    //   labelStyle: {
+    //     fontWeight: 'bold',
+    //     fontSize: 12,
+    //   },
+    // }}
     >
       <Tab.Screen
         name="Home"
         component={WelcomePage}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Tab.Screen
         name="Mentors"
@@ -75,7 +76,7 @@ function Tabs() {
       <Tab.Screen
         name="Mentees"
         component={Mentees}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       {/* <Tab.Screen
         name="HowItWorks"
@@ -107,99 +108,109 @@ const AppStack = () => {
           fontSize: 15,
         },
       }}>
-        <Drawer.Screen
+      <Drawer.Screen
         name="Home"
         component={Tabs}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="home-outline" size={22} color={color} />
           ),
         }}
       />
-       <Drawer.Screen
-         name="Profile"
-         component={Account}
+      <Drawer.Screen
+        name="Profile"
+        component={Account}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="person-outline" size={22} color={color} />
           ),
         }}
       />
       <Drawer.Screen
-       name="VideoCall"
-       component={VideoCall}
+        name="VideoCall"
+        component={VideoCall}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="md-videocam" size={22} color={color} />
           ),
         }}
       />
-     
+
       <Drawer.Screen
-     name="Help"
-component={ChatBot}
+        name="Help"
+        component={ChatBot}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-          <Drawer.Screen
-     name="Onboarding1"
-component={Onboarding1}
+      <Drawer.Screen
+        name="Onboarding1"
+        component={Onboarding1}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-        <Drawer.Screen
-     name="Onboarding2"
-component={Onboarding2}
+      <Drawer.Screen
+        name="Onboarding2"
+        component={Onboarding2}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-        <Drawer.Screen
-     name="Onboarding3"
-component={Onboarding3}
+      <Drawer.Screen
+        name="Onboarding3"
+        component={Onboarding3}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-        <Drawer.Screen
-     name="Onboarding4"
-component={Onboarding4}
+      <Drawer.Screen
+        name="Onboarding4"
+        component={Onboarding4}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-        <Drawer.Screen
-     name="Onboarding5"
-component={Onboarding5}
+      <Drawer.Screen
+        name="Onboarding5"
+        component={Onboarding5}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-        <Drawer.Screen
-     name="Onboarding6"
-component={Onboarding6}
+      <Drawer.Screen
+        name="Onboarding6"
+        component={Onboarding6}
         options={{
-          drawerIcon: ({color}) => (
+          drawerIcon: ({ color }) => (
             <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
           ),
         }}
       />
-      
-      
+      <Drawer.Screen
+        name="Careertv"
+        component={Careertv}
+        options={{
+          headerShown: false,
+          drawerIcon: ({ color }) => (
+            <Ionicons name="play-circle-outline" size={22} color={color} />
+          ),
+        }}
+      />
+
+
     </Drawer.Navigator>
   );
 };
