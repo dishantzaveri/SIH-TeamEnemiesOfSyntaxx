e84a4bb1fc384b45e9a3e3d3ce44b577d32ab3d4
grehashah6 <grehashah6@gmail.com>
2022-08-19 17:28:20 +0530

    profile navbar

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "../components/Navbar";
+import  Navbar  from "../components/Navbar";
 import { MdEmail } from "react-icons/md";
 import { FaBirthdayCake } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
