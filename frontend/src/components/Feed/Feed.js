d980aabbf3c2623b38d0545556121352118ad339
grehashah6 <grehashah6@gmail.com>
2022-08-19 17:15:57 +0530

    feed page styling

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -13,6 +13,100 @@ import { useSelector } from 'react-redux';
 import FlipMove from 'react-flip-move';
 import CreatePosts from '../CreatePosts/CreatePosts';
 import News from '../News/News';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/auth/authSlice.js";
+import { Avatar } from '@material-ui/core';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonIcon from '@material-ui/icons/Person';
+
+function HeaderOption({ avatar, Icon, title}) {
+  return (
+      <div className="headerOption">
+          {Icon && <Icon className='headerOption__icon' /> }
+          {avatar && (
+              <Avatar className="headerOption__icon" src={avatar}/>
+          )}
+          <h3 className='headerOption__title'>{title}</h3>
+      </div>
+  )
+}
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+  console.log(token);
+
+  return (
+    <div className="header px-24 py-4 flex justify-between border-b">
+      <Link className="font-bold text-3xl" to="/">
+        mentoree
+      </Link>
+{/* {token && ( */}
+  <div className="header__search">
+          <SearchIcon />
+          <input type="text" placeholder="Search bar" />
+        </div>
+{/* )} */}
+      
+
+      <div className={`flex gap-4 ${token && "hidden"} items-center`}>
+        <Link className="font-semibold text-xl" to="/registermentor">
+          Become a Mentor
+        </Link>
+        <Link className="font-semibold text-xl" to="/registermentor">
+          Find a Mentor
+        </Link>
+        <Link to="/registermentee">
+          <button className="uppercase rounded-full border w-[8vw] py-2 hover:bg-purple-gray-600 hover:text-white transition-all duration-150">
+            Sign Up
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="uppercase rounded-full border w-[8vw] py-2 hover:bg-inherit hover:text-inherit bg-purple-gray-600 text-white transition-all duration-150">
+            Log in
+          </button>
+        </Link>
+      </div>
+      <div>
+      {token && (
+        <>
+        <div className="header__right">
+        <Link to="/feed">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        </Link>
+
+        <HeaderOption Icon={SupervisorAccountIcon} title="Mentors" />
+
+        <Link to="/chat">
+        <HeaderOption Icon={ChatIcon} title="Chat" />
+        </Link>
+
+        <Link to="/myProfile">
+        <HeaderOption Icon={PersonIcon} title="Profile" />
+        </Link>
+        
+        {/* <HeaderOption Icon={NotificationsIcon} title="Notifications" /> */}
+        {/* <HeaderOption avatar={user.photoUrl} title="me" /> */}
+        <button
+          className="uppercase rounded-full border w-[8vw] py-2 hover:bg-inherit hover:text-inherit bg-purple-gray-600 text-white transition-all duration-150"
+          onClick={() => dispatch(logout())}
+        >
+          Log out
+        </button>
+      </div>
+        
+        </>
+      )}
+      </div>
+    </div>
+  );
+};
 
 function Feed() {
 //   const user = useSelector(selectUser);
@@ -43,21 +137,22 @@ function Feed() {
 //     // });
 //     setInput("");
 //   };
-  function InputOption({ Icon, title, color }) {
-    return <div className="inputOption">
-      <Icon style={{ color: color}} />
-      <h4>{title}</h4>
-    </div>;
-  };
+  
 
   return (
+  <>
+      <Header />
     <div className='grid grid-cols-12 p-8 gap-8'>
-      <div className="feed col-span-8">
+    <div className='col-span-1'>
+      </div>
+      <div className="feed col-span-6">
+
         <div className="feed_inputContainer">
           
         <CreatePosts />
           
         </div>
+       
         <FlipMove>
       
           {/* {posts.slice(0, 5).map(({ id, data: { name, description, message, photoUrl}}) => ( */}
@@ -72,10 +167,13 @@ function Feed() {
         </FlipMove>
     
       </div>
+      <div className='col-span-1'>
+      </div>
       <div className='col-span-4'>
         <News />
       </div>
     </div>
+    </>
   );
 };
 
