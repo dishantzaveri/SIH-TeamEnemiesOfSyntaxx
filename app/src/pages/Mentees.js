37ae6c8c78e66a9d90101a293838378056bf48f3
Manan17 <shahmanan170602@gmail.com>
2022-08-26 17:08:30 +0530

    Startups

--- app/src/pages/Mentees.js
+++ app/src/pages/Mentees.js
@@ -6,58 +6,105 @@ import {
   View,
   FlatList,
   StyleSheet,
-  StatusBar,
   ActivityIndicator,
+  StatusBar,
 } from 'react-native';
 import axios from 'axios';
 import {Card} from '../components/MentorMentees';
 import {SearchBar} from '../components/SearchBar';
-const Mentees = (props) => {
+import {useSelector} from 'react-redux';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+const Mentees = ({navigation}) => {
   const [person, setPerson] = useState(['']);
   const [loading, setLoading] = useState(true);
   const [text, setText] = useState('');
-
+  const [mentors, setMentors] = useState([]);
+  const {user} = useSelector(state => state.user);
   useEffect(() => {
-    getPersons();
-  }, []);
+    if (text === '') getMentorsList();
+  }, [text]);
+  const getMentorsList = async () => {
+    setLoading(true);
+    console.log(user?.token);
+    var config = {
+      method: 'get',
+      url: 'http://vismayvora.pythonanywhere.com/account/startups_list/',
+      headers: {
+        Authorization: `Token ${user?.token}`,
+        Cookie:
+          'csrftoken=o4q1Ihf3JTBVbPIRuFvCtHZVT3RHp0X8; sessionid=0rx0ut9910ocx5ggaz1l6en6khbzxg1n',
+      },
+    };
 
-  const getPersons = async () => {
-    let response = await axios.get('https://findmentor.network/persons.json');
-    setLoading(false);
-    setPerson(response.data.filter((x) => x.mentor !== 'Mentor'));
-  };
-
-  const renderItem = ({item}) => (
-    <Card data={item} navigation={props.navigation} listType="name" />
-  );
-  const filteredData = text
-    ? person.filter((item) => {
-        const itemData = item.name.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        setMentors(response.data);
+        setLoading(false);
       })
-    : person;
-  const count = Object.keys(person).length;
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const getSearchedMentors = async () => {
+    var data = new FormData();
+    data.append('expertise', 'Tech');
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', `Token ${user?.token}`);
+    myHeaders.append(
+      'Cookie',
+      'csrftoken=o4q1Ihf3JTBVbPIRuFvCtHZVT3RHp0X8; sessionid=0rx0ut9910ocx5ggaz1l6en6khbzxg1n',
+    );
+    var config = {
+      method: 'post',
+      url: 'https://vismayvora.pythonanywhere.com/account/search_startups/',
+      headers: myHeaders,
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setMentors(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const renderItem = ({item}) => {
+    return <Card navigation={navigation} data={item} />;
+  };
   return (
-    <SafeAreaView style={{flex: 1,backgroundColor: 'white',padding:10,
-    alignSelf: 'center',
- 
-    elevation: 3,
-    backgroundColor: '#fff',
-    shadowOffset: { width: 3, height: 3},
-    shadowColor: '#333',
-    shadowOpacity: 0.2,
-    shadowRadius: 1,}}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        padding: 10,
+        alignSelf: 'center',
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: {width: 3, height: 3},
+        shadowColor: '#333',
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
+      }}>
       <StatusBar backgroundColor="black" />
       <View style={styles.searchView}>
         <SearchBar
-          onSearch={(text) => setText(text)}
+          onSearch={text => setText(text)}
           value={text}
-          placeHolder="Search in entrepreneurs by name..."
+          placeHolder="Search in mentors by name..."
           placeHolderTextColor="black"
         />
+        <AntDesign
+          onPress={() => {
+            getSearchedMentors();
+          }}
+          name="search1"
+          size={30}
+        />
       </View>
-
       {loading ? (
         <View style={{marginVertical: 10}}>
           <ActivityIndicator size="large" color="#32475b" />
@@ -65,7 +112,7 @@ const Mentees = (props) => {
       ) : (
         <View style={{flex: 1}}>
           <FlatList
-            data={filteredData}
+            data={mentors}
             keyExtractor={(_, index) => index.toString()}
             renderItem={renderItem}
             numColumns={2}
@@ -84,7 +131,7 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 27,
     fontWeight: 'bold',
-    color: '#2c7cfe',
+    color: '#ffc400',
   },
   textInput: {
     textAlign: 'center',
@@ -99,6 +146,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
